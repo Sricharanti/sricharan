5ee97bc2761f9ba48b193f3f9c91ed7c8d7032bc
Leed Aguilar <leed.aguilar@ti.com>
2011-06-07 17:45:56 -0500

    gpio: adapt testsuite to the new configuration setup
    * Export specific driver configuration to general.configuration
    * Remove local configuration files: conf.sh
    * Adapt test_runner according to general.configuration
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- gpio/test_code/scripts/test_runner.sh
+++ gpio/test_code/scripts/test_runner.sh
@@ -5,6 +5,9 @@
 # Copyright remains
 #-----------------------
 
+# Global configuration file
+TS_CONF="../../utils/configuration/general.configuration"
+
 # Give standard error message and die
 die()
 {
@@ -40,19 +43,19 @@ setup()
 	}
 
 	# Load config file
-	if [ -f "./conf.sh" ]; then
-		. ./conf.sh
+	if [ -f "$TS_CONF" ]; then
+		. $TS_CONF
 		if [ $? -eq 0 ]; then
-			echo "INFO: Requested tests will be started"
+			echo "INFO: General configuration completed"
 		else
-			echo "FATAL: Configuration file with errors"
+			echo "FATAL: General configuration file with errors"
 		fi
 	else
-		die "FATAL: Configuration file not found"
+		die "FATAL: Testsuite configuration file not found"
 	fi
 
 	# scenario less tests?? have the user organize it properly at least..
-	[ -d $GPIO_DIR_SCENARIOS ] ||
+	[ -d $TC_SCENARIO ] ||
 	{
 		die "Test suite not installed correctly - no scenarios"
 	}
@@ -67,29 +70,29 @@ setup()
 usage()
 {
 	# Human redable please
-	local PP=` if [ -z "$GPIO_PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
-	local VV=` if [ -z "$GPIO_VERBOSE" ]; then echo "off"; else echo "on"; fi`
+	local PP=` if [ -z "$PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
+	local VV=` if [ -z "$VERBOSE" ]; then echo "off"; else echo "on"; fi`
 
 	# Give the gyan
 	cat <<-EOF >&2
-	usage: ./${0##*/} [-z] [-h] [-v] [-d GPIO_DIR_TEST] [-o GPIO_FILE_OUTPUT] [-l GPIO_FILE_LOG]
-	[-n GPIO_DURATION ] [-t TMPDIR] [GPIO_SCENARIO_NAMES..]
+	usage: ./${0##*/} [-z] [-h] [-v] [-d TESTDIR] [-o OUTPUTFILE] [-l LOGFILE]
+	[-n DURATION ] [-t TMPDIR] [SCENARIO_NAMES..]
 
-	-d GPIO_DIR_TEST      Run LTP to test the filesystem mounted here. [Current - $GPIO_DIR_TEST]
+	-d TESTDIR      Run LTP to test the filesystem mounted here. [Current - $TESTDIR]
 			At the end of test, the testdir gets cleaned out
-	-s GPIO_DIR_SCENARIOS  Test scenarios are located here. [Current - $GPIO_DIR_SCENARIOS]
-	-o GPIO_FILE_OUTPUT   Redirect test output to a file. [Current- $GPIO_FILE_OUTPUT {psid}]
+	-s TC_SCENARIO  Test scenarios are located here. [Current - $TC_SCENARIO]
+	-o OUTPUTFILE   Redirect test output to a file. [Current- $OUTPUTFILE {psid}]
 	-p              Human readable(dont laugh too much) format logfiles. [Current - ($PP)]
 	-z              Dont Merge the Scenario Name with tcid to create final tc id
 	-E              Use Extended Test cases also - these are painful and can take real long time
-	-l GPIO_FILE_LOG      Log results of test in a logfile. [Current- $GPIO_FILE_LOG {psid}]
-	-t TMPDIR       Run LTP using tmp dir [Current - $GPIO_DIR_TMP]
-	-n GPIO_DURATION     Execute the testsuite for given duration. Examples:
+	-l LOGFILE      Log results of test in a logfile. [Current- $LOGFILE {psid}]
+	-t TMPDIR       Run LTP using tmp dir [Current - $TMPBASE]
+	-n DURATION     Execute the testsuite for given duration. Examples:
 			-n 60s = 60 seconds
 			-n 45m = 45 minutes
 			-n 24h = 24 hours
 			-n 2d  = 2 days
-			[Current - $GPIO_DURATION]
+			[Current - $DURATION]
 
 	-v              Print more verbose output to screen.[Current - ($VV)]
 	-q              No messages from this script. no info too - Brave eh??
@@ -99,11 +102,11 @@ usage()
 			List to appear here
 	-S              Run in Stress mode
 
-	GPIO_SCENARIO_NAMES  List of scenarios to test.. else, take all scenarios
-			[Current - These are all filenames from $GPIO_DIR_SCENARIOS]
+	SCENARIO_NAMES  List of scenarios to test.. else, take all scenarios
+			[Current - These are all filenames from $TC_SCENARIO]
 
 	Good News: Ctrl+c stops and cleans up for you :)
-	More help: Read the $GPIO_ROOT/README
+	More help: Read the $TESTROOT/README
 
 	EOF
 	exit 0
@@ -115,57 +118,57 @@ sanity_check()
     # Check the current values...
     # Just ensure that pan can run with a bit of peace of mind...
 
-    [ ! -d "$GPIO_DIR_TMP" -o ! -w "$GPIO_DIR_TMP" ] && die "$GPIO_DIR_TMP - cannot work as temporary directory"
-    [ ! -d "$GPIO_DIR_TEST" -o ! -w "$GPIO_DIR_TEST" ] && die "$GPIO_DIR_TEST - cannot work as test directory"
-    [ ! -d "$GPIO_DIR_SCENARIOS" ] && die "$GPIO_DIR_SCENARIOS - No such directories"
-    [ -z "$GPIO_SCENARIO_NAMES" ] && die "No Scenarios"
-		[ ! -z "$GPIO_VERBOSE" -a ! -z "$GPIO_QUIET_MODE" ] && die "Make up your mind - verbose or quiet??"
+    [ ! -d "$TMPBASE" -o ! -w "$TMPBASE" ] && die "$TMPBASE - cannot work as temporary directory"
+    [ ! -d "$TESTDIR" -o ! -w "$TESTDIR" ] && die "$TESTDIR - cannot work as test directory"
+    [ ! -d "$TC_SCENARIO" ] && die "$TC_SCENARIO - No such directories"
+    [ -z "$SCENARIO_NAMES" ] && die "No Scenarios"
+		[ ! -z "$VERBOSE" -a ! -z "$QUIET_MODE" ] && die "Make up your mind - verbose or quiet??"
 
-    export GPIO_FILE_CMD=$GPIO_DIR_TMP/$GPIO_FILE_CMD
-    rm -f $GPIO_FILE_CMD
+    export CMDFILE=$TMPBASE/$CMDFILE
+    rm -f $CMDFILE
 
-		for SCEN in $GPIO_SCENARIO_NAMES
+		for SCEN in $SCENARIO_NAMES
     do
-		  [ ! -f "$GPIO_DIR_SCENARIOS/$SCEN" -o ! -r "$GPIO_DIR_SCENARIOS/$SCEN" ] && die "$GPIO_DIR_SCENARIOS/$SCEN - not a scenario file"
-			cat $GPIO_DIR_SCENARIOS/$SCEN|grep -v "#"|sed -e "s/^[  ]*$//g"|sed -e "/^$/d">$GPIO_FILE_TMP|| die "Count not create tmp file $GPIO_FILE_TMP"
+		  [ ! -f "$TC_SCENARIO/$SCEN" -o ! -r "$TC_SCENARIO/$SCEN" ] && die "$TC_SCENARIO/$SCEN - not a scenario file"
+			cat $TC_SCENARIO/$SCEN|grep -v "#"|sed -e "s/^[  ]*$//g"|sed -e "/^$/d">$TMPFILE|| die "Count not create tmp file $TMPFILE"
 			if [ -z "$DONT" ]; then
-				cat $GPIO_FILE_TMP|sed -e "s/^/$SCEN-/g"|sed -e "s/-/_/" >>$GPIO_FILE_CMD || die "Count not create command file $GPIO_FILE_CMD"
+				cat $TMPFILE|sed -e "s/^/$SCEN-/g"|sed -e "s/-/_/" >>$CMDFILE || die "Count not create command file $CMDFILE"
 				else
-				cat $GPIO_FILE_TMP>>$GPIO_FILE_CMD || die "Count not create command file $GPIO_FILE_CMD"
+				cat $TMPFILE>>$CMDFILE || die "Count not create command file $CMDFILE"
 			fi
 
 			# Remove the extended test cases
 			if [ -z "$EXTENDED_TEST" ]; then
 
-				cat $GPIO_FILE_CMD|grep -v "^[_A-Za-z0-9]*_EXT ">$GPIO_FILE_TMP || die "intermediate file gen failed"
-				cat $GPIO_FILE_TMP>$GPIO_FILE_CMD || die "Second intermediate creation failed"
+				cat $CMDFILE|grep -v "^[_A-Za-z0-9]*_EXT ">$TMPFILE || die "intermediate file gen failed"
+				cat $TMPFILE>$CMDFILE || die "Second intermediate creation failed"
 			fi
 
-			rm -f $GPIO_FILE_TMP
+			rm -f $TMPFILE
 
     done
 
-		local PP=` if [ -z "$GPIO_PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
-    local VV=` if [ -z "$GPIO_VERBOSE" ]; then echo "off"; else echo "on"; fi`
-    export TMPDIR=${GPIO_DIR_TEST}
+		local PP=` if [ -z "$PRETTY_PRT" ]; then echo "off"; else echo "on"; fi`
+    local VV=` if [ -z "$VERBOSE" ]; then echo "off"; else echo "on"; fi`
+    export TMPDIR=${TESTDIR}
 
 		# Print some nice info
-    if [ ! -z "$GPIO_VERBOSE" ]; then
-        debug "GPIO_POSTFIX        $GPIO_POSTFIX       "
-        info  "GPIO_ROOT       $GPIO_ROOT      "
-        info  "GPIO_DIR_TMP        $GPIO_DIR_TMP       "
-        info  "GPIO_FILE_TMP        $GPIO_FILE_TMP       "
-        debug "GPIO_FILE_CMD        $GPIO_FILE_CMD       "
-        info  "GPIO_DIR_TEST        $GPIO_DIR_TEST       "
-        info  "GPIO_PRETTY_PRT     $PP            "
-        info  "GPIO_VERBOSE        $VV            "
-        info  "GPIO_FILE_OUTPUT     $GPIO_FILE_OUTPUT    "
-        info  "GPIO_FILE_LOG        $GPIO_FILE_LOG       "
-        info  "GPIO_DURATION       $GPIO_DURATION      "
+    if [ ! -z "$VERBOSE" ]; then
+        debug "POSTFIX        $POSTFIX       "
+        info  "TESTROOT       $TESTROOT      "
+        info  "TMPBASE        $TMPBASE       "
+        info  "TMPFILE        $TMPFILE       "
+        debug "CMDFILE        $CMDFILE       "
+        info  "TESTDIR        $TESTDIR       "
+        info  "PRETTY_PRT     $PP            "
+        info  "VERBOSE        $VV            "
+        info  "OUTPUTFILE     $OUTPUTFILE    "
+        info  "LOGFILE        $LOGFILE       "
+        info  "DURATION       $DURATION      "
         debug "PATH           $PATH          "
-        info  "GPIO_DIR_SCENARIOS    $GPIO_DIR_SCENARIOS   "
+        info  "TC_SCENARIO    $TC_SCENARIO   "
         info  "TMPDIR         $TMPDIR        "
-        info  "GPIO_SCENARIO_NAMES $GPIO_SCENARIO_NAMES"
+        info  "SCENARIO_NAMES $SCENARIO_NAMES"
     fi
 }
 
@@ -175,25 +178,25 @@ main()
 	while getopts zx:Sd:qt:po:l:vn:hs:E:I arg
 	do  case $arg in
 		d)
-			GPIO_DIR_TEST=${GPIO_OPTARG} ;;
+			TESTDIR=${OPTARG} ;;
 		t)
-			GPIO_DIR_TMP=${GPIO_OPTARG} ;;
+			TMPBASE=${OPTARG} ;;
 		E)
 			EXTENDED_TEST=y ;;
 	        q)
-			GPIO_QUIET_MODE=" -q " ;;
+			QUIET_MODE=" -q " ;;
 	        z)
 			DONT=" " ;;
 		p)
-			GPIO_PRETTY_PRT=" -p " ;;
+			PRETTY_PRT=" -p " ;;
 		o)
-			GPIO_FILE_OUTPUT=${GPIO_OPTARG};OO_LOG=1 ;;
+			OUTPUTFILE=${OPTARG};OO_LOG=1 ;;
 		l)
-			GPIO_FILE_LOG=${GPIO_OPTARG} ;;
+			LOGFILE=${OPTARG} ;;
 		v)
-			GPIO_VERBOSE="-v" ;;
+			VERBOSE="-v" ;;
 		n)
-			GPIO_DURATION=" -t ${GPIO_OPTARG}" ;;
+			DURATION=" -t ${OPTARG}" ;;
 		h)
 			usage ;;
 		x)  # number of ltp's to run
@@ -205,19 +208,19 @@ main()
 			Pausing for 10 seconds...Last chance to hit that ctrl+c
 			EOF
 					sleep 10
-			GPIO_INSTANCES="-x $GPIO_OPTARG -O ${TMP}" ;;
+			INSTANCES="-x $OPTARG -O ${TMP}" ;;
 		s)
-			GPIO_DIR_SCENARIOS=${GPIO_OPTARG} ;;
+			TC_SCENARIO=${OPTARG} ;;
 		S)
-			GPIO_STRESS=y
-			GPIO_STRESSARG="-S";;
+			STRESS=y
+			STRESSARG="-S";;
 
 		\?) # Handle illegals
 			usage ;;
 
 	esac
 
-	if [ ! -z "${GPIO_OPTARG}" ]; then
+	if [ ! -z "${OPTARG}" ]; then
 		count=" $count + 2"
 	else
 		count=" $count + 1"
@@ -232,27 +235,29 @@ main()
 		count=$(($count - 1))
 	done
 
-	GPIO_SCENARIO_NAMES=$@
+	SCENARIO_NAMES=$@
 
 	sanity_check
 
 	# Test start
 
-	[ -z "$GPIO_QUIET_MODE" ] && { info "Test start time: $(date)" ; }
+	[ -z "$QUIET_MODE" ] && { info "Test start time: $(date)" ; }
+	# run pan
+	# $PAN_COMMAND #Duplicated code here, because otherwise if we fail, only "PAN_COMMAND" gets output
+	#Usage: pan -n name [ -SyAehp ] [ -s starts ] [-t time[s|m|h|d] [ -x nactive ] [
+	#-l logfile ]
+	#[ -a active-file ] [ -f command-file ] [ -d debug-level ]
+	#[-o output-file] [-O output-buffer-directory] [cmd]
 
-	# Usage: pan -n name [ -SyAehp ] [ -s starts ] [-t time[s|m|h|d] [ -x nactive ] [-l logfile ]
-	# [ -a active-file ] [ -f command-file ] [ -d debug-level ]
-	# [-o output-file] [-O output-buffer-directory] [cmd]
+	cd $TESTDIR
+	PAN_COMMAND="${UTILS_DIR_BIN}/pan $QUIET_MODE -e -S $INSTANCES $DURATION -a $$ -n $$ $PRETTY_PRT -f ${CMDFILE} -l $LOGFILE"
 
-	cd $GPIO_DIR_TEST
-	PAN_COMMAND="${UTILS_DIR_BIN}/pan $GPIO_QUIET_MODE -e -S $GPIO_INSTANCES $GPIO_DURATION -a $$ -n $$ $GPIO_PRETTY_PRT -f ${GPIO_FILE_CMD} -l $GPIO_FILE_LOG"
-
-	[ ! -z "$GPIO_VERBOSE" ] && { info "PAN_COMMAND=$PAN_COMMAND"; }
+	[ ! -z "$VERBOSE" ] && { info "PAN_COMMAND=$PAN_COMMAND"; }
 
 	if [ -z "$OO_LOG" ]; then
 		$PAN_COMMAND
 	else
-		$PAN_COMMAND|tee $GPIO_FILE_OUTPUT
+		$PAN_COMMAND|tee $OUTPUTFILE
 	fi
 
 	if [ $? -eq 0 ]; then
@@ -264,18 +269,18 @@ main()
 	fi
 
 	# Test end
-	[ -z "$GPIO_QUIET_MODE" ] && { info "Test end time: $(date)" ; }
-	[ -z "$GPIO_QUIET_MODE" ] && {
+	[ -z "$QUIET_MODE" ] && { info "Test end time: $(date)" ; }
+	[ -z "$QUIET_MODE" ] && {
 
 	cat <<-EOF >&1
 
 	###############################################################"
 		Done executing testcases."
-		Result log is in the $GPIO_FILE_LOG "
+		Result log is in the $LOGFILE "
 	###############################################################"
 
 	EOF
-	cat $GPIO_FILE_LOG
+	cat $LOGFILE
 
 	}
 	cleanup
@@ -285,14 +290,14 @@ main()
 
 cleanup()
 {
-	[  -z "$GPIO_QUIET_MODE" ] && echo -n "INFO: Cleaning up..."
-	if [ -n "${GPIO_FILE_TMP}" -a -n "${GPIO_FILE_CMD}" -a -n "${GPIO_DIR_TEST}" -a -n "${GPIO_DIR_TMP}" ]; then
-		rm -rf ${GPIO_FILE_TMP} ${GPIO_FILE_CMD} ${GPIO_DIR_TEST}/* ${GPIO_DIR_TMP}/*
+	[  -z "$QUIET_MODE" ] && echo -n "INFO: Cleaning up..."
+	if [ -n "${TMPFILE}" -a -n "${CMDFILE}" -a -n "${TESTDIR}" -a -n "${TMPBASE}" ]; then
+		rm -rf ${TMPFILE} ${CMDFILE} ${TESTDIR}/* ${TMPBASE}/*
 	else
 		echo "INFO: Clean up process won't be executed because variables for directories to be removed are not set..."
 	fi
 
-	[  -z "$GPIO_QUIET_MODE" ] && echo "done."
+	[  -z "$QUIET_MODE" ] && echo "done."
 }
 
 
