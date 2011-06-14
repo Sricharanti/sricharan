057ed83050431fd9aa254205c00e93fc8d4c99b8
Leed Aguilar <leed.aguilar@ti.com>
2011-06-14 10:06:36 -0500

    i2c: report i2c proper i2c bus1 speed in scenarios
    Now, the i2c bus2 speed information is reported properly:
    
    "Working with Bus 1 with speed 400 kHz"
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- i2c/test_code/scripts/helper/verifyspeed.sh
+++ i2c/test_code/scripts/helper/verifyspeed.sh
@@ -2,11 +2,12 @@
 
 system_message_buffer.sh "system.log" || exit 1
 I2C_ADAPTER_INFO=`cat "system.log" | grep i2c | grep "bus 1"`
-SPEED_ACTUAL=`echo $I2C_ADAPTER_INFO | sed -e "s/ */ /g" | cut -d ' ' -f8`
+SPEED_ACTUAL=`echo $I2C_ADAPTER_INFO | sed -e "s/ */ /g" | cut -d ' ' -f10`
+SPEED_UNITS=`echo $I2C_ADAPTER_INFO  | sed -e "s/ */ /g" | cut -d ' ' -f11`
 rm "system.log" || exit 1
 
 echo ""
-echo "Working with Bus 1 with speed $SPEED_ACTUAL"
+echo "Working with Bus 1 with speed $SPEED_ACTUAL $SPEED_UNITS"
 echo "$I2C_ADAPTER_INFO"
 echo ""
 
