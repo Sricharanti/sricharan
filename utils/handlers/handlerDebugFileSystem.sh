9b5bd4f23d4bf3718ab20c2bbe5052f01deee489
Leed Aguilar <leed.aguilar@ti.com>
2011-08-07 17:20:40 -0500

    Handler Debug FS script clean up and add error handling
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- utils/handlers/handlerDebugFileSystem.sh
+++ utils/handlers/handlerDebugFileSystem.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
+#  Copyright (c) 2011 Texas Instruments
+#
+#  Author: Abraham Arce <x0066660@ti.com>
+#  Author: Leed Aguilar <leed.aguilar@ti.com>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
+
 # =============================================================================
 # Variables
 # =============================================================================
 
-LOCAL_COMMAND=$1
+command=$1
+debugfs_entry_value=$2
+debugfs_entry_name=$3
 
 # =============================================================================
 # Functions
@@ -21,37 +44,48 @@ if [ $? -eq 1 ]; then
 	exit 1
 fi
 
-if [ "$LOCAL_COMMAND" = "mount" ]; then
+if [ "$command" = "mount" ]; then
 
-	test -d $HDFS_DEBUG_FILESYSTEM_DIRECTORY || mkdir -p $HDFS_DEBUG_FILESYSTEM_DIRECTORY
-
-	mount | grep debugfs | grep "debugfs on $HDFS_DEBUG_FILESYSTEM_DIRECTORY"
-
-	if [ $? != 0 ]; then
-		mount -t debugfs debugfs $HDFS_DEBUG_FILESYSTEM_DIRECTORY
+	# Verify/Create debugfs mount point
+	test -d $PM_DEBUGFS_DIRECTORY || mkdir -p $PM_DEBUGFS_DIRECTORY
+	mount | grep debugfs | grep "$PM_DEBUGFS_DIRECTORY on $PM_DEBUGFS_DIRECTORY"
+	if [ $? -ne 0 ]; then
+		# mount debugfs
+		mount -t debugfs debugfs $PM_DEBUGFS_DIRECTORY
+		if [ $? -ne 0 ]; then
+			echo "[handlerDebugFS] Error: debugfs can not be mounted"
+			handlerError.sh "log" "1" "halt" "handlerDebugFileSystem.sh"
+			exit 1
+		else
+			echo "[handlerDebugFS] debugfs was successfully mounted"
+		fi
 	fi
 
-elif [ "$LOCAL_COMMAND" = "set" ]; then
-
-	LOCAL_DEBUGFS_ENTRY_VALUE=$2
-	LOCAL_DEBUGFS_ENTRY_NAME=$3
+elif [ "$command" = "set" ]; then
 
 	handlerDebugFileSystem.sh "mount"
-
-	test -f $LOCAL_DEBUGFS_ENTRY_NAME
-	if [ $? != 0 ]; then
-		handlerError.sh "log" "1" "halt" "handlerSysFs.sh"
+	test -f $debugfs_entry_name
+	if [ $? -ne 0 ]; then
+		echo "[handlerDebugFS] Error: < $debugfs_entry_name > doesn't exist"
+		handlerError.sh "log" "1" "halt" "handlerDebugFileSystem.sh"
 		exit 1
 	fi
 
-	echo $LOCAL_DEBUGFS_ENTRY_VALUE > $LOCAL_DEBUGFS_ENTRY_NAME
+	handlerSysFs.sh "set" $debugfs_entry_name $debugfs_entry_value
+	handlerSysFs.sh "verify" $debugfs_entry_name $debugfs_entry_value
+	if [ $? -ne 0 ]; then
+		echo "[handlerDebugFS] Error: Not able to write into $debugfs_entry_name"
+		handlerError.sh "log" "1" "halt" "handlerDebugFileSystem.sh"
+		exit 1
+	fi
 
-	handlerDebugFileSystem.sh "umount"
+elif [ "$command" = "umount" ]; then
 
-elif [ "$LOCAL_COMMAND" = "umount" ]; then
-
-	umount $HDFS_DEBUG_FILESYSTEM_DIRECTORY
-
+	umount $PM_DEBUGFS_DIRECTORY
+	if [ $? -ne 0 ]; then
+		echo "[handlerDebugFS] Error: Unable to umount debugfs"
+		exit 1
+	fi
 fi
 
 # End of file
