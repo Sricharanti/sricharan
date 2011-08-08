e7124ad659065641198a67e7e0499e22faa4b2ff
Leed Aguilar <leed.aguilar@ti.com>
2011-08-08 02:28:03 -0500

    handlers: CPU governor handler cosmetic changes
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- utils/handlers/handlerCpuFreqScalGovernors.sh
+++ utils/handlers/handlerCpuFreqScalGovernors.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
+#
+#  CPU Governor Handler
+#
+#  Copyright (c) 2010 Texas Instruments
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
+
 # =============================================================================
 # Variables
 # =============================================================================
 
-LOCAL_OPERATION=$1
+operation=$1
+cpugovernor=$2
+command_line=$3
 
 # =============================================================================
 # Functions
@@ -12,98 +35,88 @@ LOCAL_OPERATION=$1
 
 setOneGovernor() {
 
-	LOCAL_GOVERNOR=$1
-	LOCAL_COMMAND_LINE=$2
+	local_governor=$1
+	command_to_execute=$2
 
-	if [ -n "$LOCAL_COMMAND_LINE" ]; then
-		eval $LOCAL_COMMAND_LINE &
+	if [ -n "$command_to_execute" ]; then
+		eval $command_to_execute &
 	fi
 
-	echo $LOCAL_GOVERNOR > $SYSFS_CPU0_CURRENT_GOVERNOR
-	if [ $? -eq 1 ]; then
-		showInfo "Error: Governor < $LOCAL_GOVERNOR > cannot be set"
+	handlerSysFs.sh "set" $SYSFS_CPU0_CURRENT_GOVERNOR $local_governor
+	handlerSysFs.sh "verify" $SYSFS_CPU0_CURRENT_GOVERNOR $local_governor
+	if [ $? -ne 0 ]; then
+		showInfo "Error: Governor < $local_governor > cannot be set"
 		exit 1
-	else
-		showInfo "Governor < $LOCAL_GOVERNOR > was set correctly"
 	fi
-
+	showInfo "Governor < $local_governor > was set correctly"
 	wait
-
 	sleep 5
 }
 
 setAllGovernor() {
 
-	LOCAL_COMMAND_LINE=$@
-
+	command_to_execute=$@
 	error=0
 	echo > $HCFSG_GOVERNORS_LIST_OK
 	echo > $HCFSG_GOVERNORS_LIST_ERROR
 
-	LOCAL_GOVERNORS_LIST_AVAILABLE=`cat $SYSFS_CPU0_AVAILABLE_GOVERNORS`
-	showInfo "Info: Available Governors are -> $LOCAL_GOVERNORS_LIST_AVAILABLE"
-
-	if [ -n "$LOCAL_COMMAND_LINE" ]; then
-		$LOCAL_COMMAND_LINE &
-		LOCAL_COMMAND_PID=`echo $!`
+	available_governors=`cat $SYSFS_CPU0_AVAILABLE_GOVERNORS`
+	showInfo "Info: Available Governors are -> $available_governors"
+	if [ -n "$command_to_execute" ]; then
+		eval $command_to_execute &
+		command_pid=`echo $!`
 	fi
-
 	while [ 1 ]; do
-
-		for i in $LOCAL_GOVERNORS_LIST_AVAILABLE
-
-		do
-			showInfo "Setting Governor to $i"
-			echo $i > $SYSFS_CPU0_CURRENT_GOVERNOR
-			if [ $? -eq 1 ]; then
-				showInfo "Error: Governor $i cannot be set"
-				echo $i >> $HCFSG_GOVERNORS_LIST_ERROR
+		for governor in $available_governors; do
+			showInfo "Setting CPU Governor to $governor"
+			handlerSysFs.sh "set" $SYSFS_CPU0_CURRENT_GOVERNOR $governor
+			handlerSysFs.sh "verify" $SYSFS_CPU0_CURRENT_GOVERNOR $governor
+			if [ $? -ne 0 ]; then
+				showInfo "Error: Governor $governor cannot be set"
+				echo $governor >> $HCFSG_GOVERNORS_LIST_ERROR
 				error=1
 			else
-				showInfo "Info: Governor $i was correctly set"
-				echo $i >> $HCFSG_GOVERNORS_LIST_OK
+				showInfo "Info: Governor $governor was set correctly"
+				echo $governor >> $HCFSG_GOVERNORS_LIST_OK
 			fi
+			# TODO: Add delay option between setting cycles
 			sleep 1
 		done
 
-		if [ -n "$LOCAL_COMMAND_LINE" ]; then
-			test -d /proc/$LOCAL_COMMAND_PID || break
+		if [ -n "$command_to_execute" ]; then
+			test -d /proc/$command_pid || break
 		else
 			break
 		fi
-
 	done
-
 	wait
 
-	showInfo "Info: The following Governors were correctly set"
+	showInfo "Info: The following Governors were set correctly"
 	cat $HCFSG_GOVERNORS_LIST_OK
-	showInfo "Info: The following Governors were not correctly set"
-	cat $HCFSG_GOVERNORS_LIST_ERROR
-
-	sleep 5
-
 	if [ $error -eq 1 ]; then
+		showInfo "Info: The following Governors were not set correctly"
+		cat $HCFSG_GOVERNORS_LIST_ERROR
 		exit 1
 	fi
 }
 
 getCurrentGovernor() {
-	LOCAL_FILE=$1
-	LOCAL_GOVERNOR=`cat $SYSFS_CPU0_CURRENT_GOVERNOR`
-	showInfo "Info: Current Governor -> $LOCAL_GOVERNOR"
+	governor_saved=$1
+	current_governor=`cat $SYSFS_CPU0_CURRENT_GOVERNOR`
+	showInfo "Info: Current Governor -> $current_governor"
 
-	echo $LOCAL_GOVERNOR > $LOCAL_FILE
+	echo $current_governor > $governor_saved
 }
 
 restoreCurrentGovernor() {
-	LOCAL_FILE=$1
+	governor_saved=$1
 
-	if [ -f $LOCAL_FILE ]; then
-		cat $LOCAL_FILE > $SYSFS_CPU0_CURRENT_GOVERNOR
-		showInfo "Info: Restore to Governor -> `cat $LOCAL_FILE`"
+	if [ -f "$governor_saved" ]; then
+		# TODO: Verify that the cpu governor was set correctly
+		cat $governor_saved > $SYSFS_CPU0_CURRENT_GOVERNOR
+		showInfo "Info: Restore to Governor -> `cat $governor_saved`"
 	else
-		showInfo 'Error: $LOCAL_FILE parameter is empty'
+		showInfo 'Error: $governor_saved file is empty'
 	fi
 }
 
@@ -120,48 +133,45 @@ if [ $? -eq 1 ]; then
 	exit 1
 fi
 
+# Verify that all required syfs entries are available
 if [ ! -f $SYSFS_CPU0_AVAILABLE_GOVERNORS ]; then
 	showInfo "FATAL: $SYSFS_CPU0_AVAILABLE_GOVERNORS cannot be found!"
 	handlerError.sh "log" "1" "halt" "handlerCpuFreqScalGovernors.sh"
 	exit 1
 fi
-
 if [ ! -f $SYSFS_CPU0_CURRENT_GOVERNOR ]; then
 	showInfo "FATAL: $SYSFS_CPU0_CURRENT_GOVERNOR cannot be found!"
 	handlerError.sh "log" "1" "halt" "handlerCpuFreqScalGovernors.sh"
 	exit 1
 fi
 
-if [ "$LOCAL_OPERATION" = "list" ]; then
+# TODO: Add script usage
 
-  cat $SYSFS_CPU0_AVAILABLE_GOVERNORS
+if [ "$operation" = "list" ]; then
 
-elif [ "$LOCAL_OPERATION" = "set" ]; then
+	cat $SYSFS_CPU0_AVAILABLE_GOVERNORS
 
-	LOCAL_GOVERNOR=$2
+elif [ "$operation" = "set" ]; then
 
-	if [ "$LOCAL_GOVERNOR" = "all" ]; then
+	if [ "$cpugovernor" = "all" ]; then
 		setAllGovernor
 	else
-		setOneGovernor $LOCAL_GOVERNOR
+		setOneGovernor $cpugovernor
 	fi
 
-elif [ "$LOCAL_OPERATION" = "run" ]; then
+elif [ "$operation" = "run" ]; then
 
-	LOCAL_GOVERNOR=$2
-	LOCAL_COMMAND_LINE=$3
-
-	if [ "$LOCAL_GOVERNOR" = "all" ]; then
-		setAllGovernor "$LOCAL_COMMAND_LINE"
+	if [ "$cpugovernor" = "all" ]; then
+		setAllGovernor "$command_line"
 	else
-		setOneGovernor $LOCAL_GOVERNOR "$LOCAL_COMMAND_LINE"
+		setOneGovernor $cpugovernor "$command_line"
 	fi
 
-elif  [ "$LOCAL_OPERATION" = "get" ]; then
+elif  [ "$operation" = "get" ]; then
 
 	getCurrentGovernor $HCFSG_CURRENT_GOVERNOR_FILE
 
-elif  [ "$LOCAL_OPERATION" = "restore" ]; then
+elif  [ "$operation" = "restore" ]; then
 
 	restoreCurrentGovernor $HCFSG_CURRENT_GOVERNOR_FILE
 
