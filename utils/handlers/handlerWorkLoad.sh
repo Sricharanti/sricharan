a7b6d9f0f132db152961123a05f32187be4b11bc
Leed Aguilar <leed.aguilar@ti.com>
2011-08-22 20:15:14 -0500

    handlers: WorkLoad: Modify script functionality
    Launch a work load during a specific amount of time.
    
    Usage: ./handlerWorkLoad.sh <start> [time in seconds]
    
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- utils/handlers/handlerWorkLoad.sh
+++ utils/handlers/handlerWorkLoad.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 
-set -x
+
+# HandlerWorkLoad.sh provides a cpu load of ~100%
+
+#  Copyright (c) 2011 Texas Instruments
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+#  USA
+#
 
 # =============================================================================
 # Variables
 # =============================================================================
 
-LOCAL_OPERATION=$1
+operation=$1
+duration=$2 # in seconds
 
 # =============================================================================
 # Functions
@@ -23,15 +43,11 @@ if [ $? -eq 1 ]; then
   exit 1
 fi
 
-if [ "$LOCAL_OPERATION" = "start" ]; then
-
+if [ "$operation" = "start" ]; then
 	nohup dd if=/dev/urandom of=/dev/null &
-
-elif [ "$LOCAL_OPERATION" = "stop" ]; then
-
-	pid=`ps -x | grep dd | grep /dev/null | awk '{print $1}'`
+	pid=`echo $!`
+	sleep $duration
 	kill $pid
-
 fi
 
 # End of file
