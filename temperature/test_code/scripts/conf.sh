1e4dd3b7192b6de9d677bf05b2ab31ead7153088
David Taylor <x0158378@ti.com>
2011-11-15 22:33:12 -0600

    sensors: temperature: added test files derived from sfh7741
    Signed-off-by: David Taylor <x0158378@ti.com>

---
+++ temperature/test_code/scripts/conf.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# TestSuite General Variables
+export SENSOR_POSTFIX=`date "+%Y%m%d-%H%M%S"`
+export SENSOR_ROOT=`pwd`
+
+export SENSOR_DIR_BINARIES=${SENSOR_ROOT}/../bin
+export SENSOR_DIR_HELPER=${SENSOR_ROOT}/helper
+export SENSOR_DIR_TMP=${SENSOR_ROOT}/tmp
+export SENSOR_DIR_TEST=${SENSOR_ROOT}/test
+export SENSOR_DIR_SCENARIOS="${SENSOR_ROOT}/scenarios"
+
+export SENSOR_FILE_OUTPUT=${SENSOR_ROOT}/output.$SENSOR_POSTFIX
+export SENSOR_FILE_LOG=${SENSOR_ROOT}/log.$SENSOR_POSTFIX
+export SENSOR_FILE_TMP=${SENSOR_DIR_TMP}/tmp.$SENSOR_POSTFIX
+export SENSOR_FILE_CMD=cmd.$SENSOR_POSTFIX
+
+export SENSOR_DURATION=""
+export SENSOR_PRETTY_PRT=""
+export SENSOR_VERBOSE=""
+export SENSOR_SCENARIO_NAMES=""
+export SENSOR_STRESS=""
+
+export PATH="${PATH}:${SENSOR_ROOT}:${SENSOR_DIR_BINARIES}:${SENSOR_DIR_HELPER}"
+
+# Utils General Variables
+export UTILS_DIR=$SENSOR_ROOT/../../utils/
+export UTILS_DIR_BIN=$UTILS_DIR/bin
+export UTILS_DIR_HANDLERS=$UTILS_DIR/handlers
+export UTILS_DIR_SCRIPTS=$UTILS_DIR/scripts
+
+. $UTILS_DIR/configuration/general.configuration
+
+export PATH="$PATH:$UTILS_DIR_BIN:$UTILS_DIR_HANDLERS:$UTILS_DIR_SCRIPTS"
+
+# General variables
+export DMESG_FILE=/var/log/dmesg
+
+
+export DEVFS_TEMP=/sys/class/hwmon/hwmon0/device/temp1_input
+
+# End of file
---
+++ temperature/test_code/scripts/helper/Readvalues.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+i=$1
+sleepno=$2
+if [ -z "$sleepno" ]; then
+	sleepno=0
+fi
+while [ 0 -le $i ]
+do
+  cat $DEVFS_TEMP
+  RET=$?
+  if [ "$RET" = "0" ]; then
+  	  #wait for some time then take another reading
+  	  echo -n "."
+  	  sleep $sleepno;
+  else
+  	  echo "error"
+  	  exit $?
+  fi
+  i=`expr $i - 1`
+done
+exit 0
+
