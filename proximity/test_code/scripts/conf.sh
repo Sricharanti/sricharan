887f2fbb16a1268d8b7a0ff1618db35d77a19694
David Taylor <x0158378@ti.com>
2011-11-15 20:29:46 -0600

    sensors: proximity: hardware / input event setup/detection
    Signed-off-by: David Taylor <x0158378@ti.com>

--- proximity/test_code/scripts/conf.sh
+++ proximity/test_code/scripts/conf.sh
@@ -29,20 +29,28 @@ export UTILS_DIR_BIN=$UTILS_DIR/bin
 export UTILS_DIR_HANDLERS=$UTILS_DIR/handlers
 export UTILS_DIR_SCRIPTS=$UTILS_DIR/scripts
 
-. $UTILS_DIR/configuration/general.configuration
-
 export PATH="$PATH:$UTILS_DIR_BIN:$UTILS_DIR_HANDLERS:$UTILS_DIR_SCRIPTS"
+
+. $UTILS_DIR/configuration/general.configuration
+if [ `cat $SYSFS_BOARD_REV | grep -c "Tablet"` -ge 1 ]; then
+	#specific to tsl2771 sensor
+	export PROXIMITY_HW="tsl2771"
+	export PROXIMITY_INPUTDEV="tsl2771_prox"
+	export PROXIMITY_SYSFS_PATH="/sys/bus/i2c/drivers/tsl2771/4-0039"
+	export PROXIMITY_ENABLE="$PROXIMITY_SYSFS_PATH/prox_enable"
+elif [ `cat $SYSFS_BOARD_REV | grep -wc "Blaze/SDP"` -ge 1  ]; then
+	#specific to sfh7741 sensor
+	export PROXIMITY_HW="sfh7741"
+	export PROXIMITY_INPUTDEV="sfh7741"
+else
+	echo "Warning: Unrecognized hardware platform"
+	exit 1
+fi
 
 # General variables
 export DMESG_FILE=/var/log/dmesg
-export DIGITAL_COMPASS_SYSFS=/sys/devices/platform/i2c_omap.4/i2c-4/4-001e/device0
-export DIGITAL_COMPASS_OM=$DIGITAL_COMPASS_SYSFS/operating_mode
-export DIGITAL_COMPASS_X_AXIS=$DIGITAL_COMPASS_SYSFS/magn_x_raw
-export DIGITAL_COMPASS_Y_AXIS=$DIGITAL_COMPASS_SYSFS/magn_y_raw
-export DIGITAL_COMPASS_Z_AXIS=$DIGITAL_COMPASS_SYSFS/magn_z_raw
-export DIGITAL_COMPASS_ALL_AXIS=$DIGITAL_COMPASS_SYSFS/magn_*_raw
-export DIGITAL_COMPASS_SINGLE_MODE=1
-export DIGITAL_COMPASS_MULTI_MODE=0
+
+$UTILS_DIR_SCRIPTS/mknodins.sh
 
 # Sensor devfs node
 TEMP_EVENT=`ls /sys/class/input/ | grep event`
@@ -50,7 +58,7 @@ set $TEMP_EVENT
 
 for i in $TEMP_EVENT
 do
-	cat /sys/class/input/$i/device/name | grep "sfh7741"
+	cat /sys/class/input/$i/device/name | grep $PROXIMITY_INPUTDEV
 	IS_THIS_OUR_DRIVER=`echo $?`
 	if [ "$IS_THIS_OUR_DRIVER" -eq "0" ]
 	then
@@ -59,11 +67,6 @@ do
 	fi
 done
 
-# Hardcode primary controller for now
-#export DEVFS_SENSOR=/dev/input/event3
-export DEVFS_TEMP=/sys/class/hwmon/hwmon0/device/temp1_input
-export DEVFS_BMP085_TEMP=/sys/class/i2c-adapter/i2c-4/4-0077/temp0_input
-export DEVFS_BMP085_PRESS=/sys/class/i2c-adapter/i2c-4/4-0077/pressure0_input
 if [ ! -e "$DEVFS_SENSOR" ]
 then
 	echo "FATAL: Proximity node cannot be found -> $DEVFS_SENSOR"
