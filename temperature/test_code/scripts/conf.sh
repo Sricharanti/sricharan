ddc73395c3dcc6753075258f3cd03c5654fb973b
Andrii Danylov <x0158318@ti.com>
2012-11-06 15:18:22 +0200

    Temperature:omap5 configuration providing.
    This patch provide omap5 configuration for bmp085 sensor.
    
    Signed-off-by: Andrii Danylov <x0158318@ti.com>

--- temperature/test_code/scripts/conf.sh
+++ temperature/test_code/scripts/conf.sh
@@ -36,7 +36,12 @@ export PATH="$PATH:$UTILS_DIR_BIN:$UTILS_DIR_HANDLERS:$UTILS_DIR_SCRIPTS"
 # General variables
 export DMESG_FILE=/var/log/dmesg
 
-
+if [ `cat /proc/cpuinfo| grep -ic OMAP4` -ne 0 ];then
 export DEVFS_TEMP=/sys/class/hwmon/hwmon0/device/temp1_input
+elif [ `cat /proc/cpuinfo| grep -ic OMAP5` -ne 0 ];then
+export DEVFS_TEMP=/sys/bus/i2c/drivers/bmp085/2-0077/temp0_input
+else
+echo "Unknown device, please provide configuration."
+fi
 
 # End of file
