56440c3ae75f44d0a7041b0ca82a25bed388e3d0
Andrii Danylov <x0158318@ti.com>
2013-01-31 20:38:03 +0200

    timer:remove uninitialated variable from if.
    RESERVED_GPTIMER can not be initialated from boot log
    because reserved timers do not provide log any more.
    
    Signed-off-by: Andrii Danylov <x0158318@ti.com>

--- timer-32k/test_code/scripts/helper/requestTimerNumber.sh
+++ timer-32k/test_code/scripts/helper/requestTimerNumber.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 REQUESTED_GPTIMER=$1
 
-if [ $REQUESTED_GPTIMER -eq $RESERVED_GPTIMER ]; then
-	echo "A GPtimer was requested when it was already reserved."
-	echo "Module insertion is not going to be performed"
-	exit 0
-fi
-
 if [ `cat /proc/cpuinfo | grep -wc "OMAP4\|OMAP5"` -ge 1 ]; then
 	if [ $REQUESTED_GPTIMER -gt 4 ] && [ $REQUESTED_GPTIMER -lt 9 ]; then
 		echo "GPtimers 5-8 are not available"
