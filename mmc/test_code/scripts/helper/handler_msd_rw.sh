44a1bacd29e29f9fa5ef8a8b2b1939f7cc406daf
Andrii Danylov <x0158318@ti.com>
2013-02-07 17:54:39 +0200

    mmc:handler_msd_rw.sh modification for stable work.
    Fix issues in dvfs and off mode scenarios.
    
    Signed-off-by: Andrii Danylov <x0158318@ti.com>

--- mmc/test_code/scripts/helper/handler_msd_rw.sh
+++ mmc/test_code/scripts/helper/handler_msd_rw.sh
@@ -9,17 +9,22 @@
 # Date:		02/04/2013
 ########################################################
 
-FOLDER=$1
-RW_OP=$2
+RW_OP=$1
+FOLDER=$2
 
 for (( i=0; i<8; i++ )); do
+
+	if [ "$RW_OP" = "1" ]; then
 	echo 1 > /proc/sys/vm/drop_caches && sync
-	if [ RW_OP -eq "1" ];	#write op
 	dd if=/dev/zero of=$FOLDER/tmp.file bs=1024000 count=5
-	else			#read
+	elif [ "$RW_OP" = "0" ]; then
 	dd if=$FOLDER/tmp.file of=/dev/null bs=1024000 count=5
-	rm $FOLDER/tmp.file
 	fi
 	sleep 0.5
 done
+
+if [ "$RW_OP" = "2" ]; then
+rm $FOLDER/tmp.file
+fi
+
 exit 0
