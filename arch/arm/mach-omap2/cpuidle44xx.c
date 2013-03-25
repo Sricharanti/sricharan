0c90393d630deaaf23eccf99a6e1accc40c2b6a8
Santosh Shilimkar <santosh.shilimkar@ti.com>
2013-03-25 15:35:04 +0530

    ARM: OMAP4: CPUidle: Avoid double idle driver registration
    OMAP4 CPUidle driver registration call is under a loop which leads
    to calling cpuidle_register_driver twice which is not intended.
    
    Fix it by moving the driver registration outside the loop.
    
    Reported-by: Nishanth Menon <nm@ti.com>
    Acked-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
    Signed-off-by: Kevin Hilman <khilman@linaro.org>

--- arch/arm/mach-omap2/cpuidle44xx.c
+++ arch/arm/mach-omap2/cpuidle44xx.c
@@ -239,14 +239,14 @@ int __init omap4_idle_init(void)
 	/* Configure the broadcast timer on each cpu */
 	on_each_cpu(omap_setup_broadcast_timer, NULL, 1);
 
+	cpuidle_register_driver(&omap4_idle_driver);
+
 	for_each_cpu(cpu_id, cpu_online_mask) {
 		dev = &per_cpu(omap4_idle_dev, cpu_id);
 		dev->cpu = cpu_id;
 #ifdef CONFIG_ARCH_NEEDS_CPU_IDLE_COUPLED
 		dev->coupled_cpus = *cpu_online_mask;
 #endif
-		cpuidle_register_driver(&omap4_idle_driver);
-
 		if (cpuidle_register_device(dev)) {
 			pr_err("%s: CPUidle register failed\n", __func__);
 			return -EIO;
