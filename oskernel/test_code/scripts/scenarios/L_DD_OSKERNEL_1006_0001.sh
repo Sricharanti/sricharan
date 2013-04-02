9718489a7bc47c85457c45f6bc9b87b85ff5536d
Andrii Danylov <x0158318@ti.com>
2013-04-02 17:45:14 +0300

    oskernel:emif source check modification for new kernel.
    This patch check emif.h file with new path and line with other content.
    
    Signed-off-by: Andrii Danylov <x0158318@ti.com>

--- oskernel/test_code/scripts/scenarios/L_DD_OSKERNEL_1006_0001.sh
+++ oskernel/test_code/scripts/scenarios/L_DD_OSKERNEL_1006_0001.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -x
 
-	KDIR_EMIF="$KDIR/arch/arm/mach-omap2/include/mach/emif.h"
-	emif=`cat $KDIR_EMIF | grep "define EMIF_NUM_INSTANCES" | sed "s/\#define EMIF_NUM_INSTANCES //"`
-	if [ "$emif" != "2" -o $? -ne 0 ]; then
+	KDIR_EMIF="$KDIR/drivers/misc/emif.h"
+	emif=`cat $KDIR_EMIF | grep "define EMIF_MAX_NUM_FREQUENCIES" | awk '{print $3}'`
+	if [ "$emif" != "6" -o $? -ne 0 ]; then
 	    echo "EMIF: $emif"
 	    echo "INFO: Test FAIL" && exit 1
 	else
 	    echo "EMIF: $emif"
 	    echo "INFO: Test PASS" && exit 0
 	fi
-
-
