688b8fb116bc88a4d300185d11056de92b041152
Mugunthan V N <mugunthanvnm@ti.com>
2013-10-25 16:26:45 +0530

    ARM: AM43xx: clocks: Enable CPGMAC clock control
    Enable CPGMAC clock control for AM43xx to use ethernet in U-Boot
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- arch/arm/cpu/armv7/am33xx/clock_am43xx.c
+++ arch/arm/cpu/armv7/am33xx/clock_am43xx.c
@@ -95,6 +95,7 @@ void enable_basic_clocks(void)
 		&cmper->gpio2clkctrl,
 		&cmper->gpio3clkctrl,
 		&cmper->i2c1clkctrl,
+		&cmper->cpgmac0clkctrl,
 		&cmper->emiffwclkctrl,
 		&cmper->emifclkctrl,
 		&cmper->otfaemifclkctrl,
