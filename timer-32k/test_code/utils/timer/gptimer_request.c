fb7af2ffdcaeaacf8a8abe79a1d49d7f58cb3e02
Sricharan R <r.sricharan@ti.com>
2014-05-05 17:23:46 +0530

    Add cb test case
    Signed-off-by: Sricharan R <r.sricharan@ti.com>

--- timer-32k/test_code/utils/timer/gptimer_request.c
+++ timer-32k/test_code/utils/timer/gptimer_request.c
@@ -4,95 +4,209 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/version.h>
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 27) && LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 31))
-#include <mach/dmtimer.h>
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 32)
-#include <plat/dmtimer.h>
-#else
-#include <asm/arm/dmtimer.h>
-#endif
 #include <linux/clk.h>
 #include <linux/types.h>
 #include <linux/io.h>
+#include <linux/irqdesc.h>
+#include <linux/irqdomain.h>
+#include <linux/debugfs.h>
+#include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/platform_device.h>
+#include <linux/of_irq.h>
 
-MODULE_DESCRIPTION("GP Timer Request and Reserve Test Module");
-MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("CB TEST MODULE");
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("SRICHARAN");
 
-/*Pointer to timer struct*/
-static struct omap_dm_timer *timer_ptr;
+#define MAX_IRQS 192
 
-/*The IRQ # for the current gp timer*/
-static int32_t timer_irq;
+static struct platform_device *platdev[MAX_IRQS];
+static int used_irqs[MAX_IRQS];
+static struct irq_desc *descs[MAX_IRQS];
+static struct dentry *d;
+static u32 omap_cb_test_all;
+static u32 stress;
 
-/*Clock type to use*/
-static uint clock_type;
+static const struct of_device_id omap_cb_match[] = {
+	{ .compatible = "ti,irq-crossbar-test", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, omap_cb_match);
 
-module_param(clock_type, int, S_IRUGO|S_IWUSR);
-
-static int __init gptimer_request_init(void)
+static int delete_allocation(void *unused)
 {
-	struct clk *gt_fclk;
-	uint32_t gt_rate;
+	int i = 0;
+	struct irq_desc *desc;
 
-	/*Requesting for any available timer*/
-	timer_ptr = omap_dm_timer_request();
+	printk("\n delete_allocation");
+	for (i = 0; i < (MAX_IRQS-32); i++)
+	{
+		if (used_irqs[i])
+			continue;
 
-	if (timer_ptr == NULL) {
-		/*Timers are not available*/
-		printk(KERN_ERR "GPtimers are not available\n");
-		return -1;
+		desc = descs[i];
+
+		if (desc) {
+			irq_dispose_mapping(desc->irq_data.irq);
+			descs[i] = 0;
+		}
 	}
 
-	switch (clock_type) {
-	case 1:
-		/*Set the clock source to 32Khz Clock*/
-		printk(KERN_INFO "Using OMAP_TIMER_SRC_32_KHZ source\n");
-		omap_dm_timer_set_source(timer_ptr,
-			OMAP_TIMER_SRC_32_KHZ);
-		break;
-	case 2:
-		/*Set the clock source to System Clock*/
-		printk(KERN_INFO "Using OMAP_TIMER_SRC_SYS_CLK source\n");
-		omap_dm_timer_set_source(timer_ptr,
-			OMAP_TIMER_SRC_SYS_CLK);
-		break;
-	case 3:
-		/*Set the clock source to System Clock*/
-		printk(KERN_INFO "Using OMAP_TIMER_SRC_EXT_CLK source\n");
-		omap_dm_timer_set_source(timer_ptr,
-			OMAP_TIMER_SRC_EXT_CLK);
-		break;
-	default:
-		printk(KERN_ERR "Invalid clock_type value. Use 1, 2 or 3\n");
-		return -1;
-	}
-
-	/*Figure out what IRQ our timer triggers*/
-	timer_irq = omap_dm_timer_get_irq(timer_ptr);
-
-	/*Get clock rate in Hz*/
-	gt_fclk = omap_dm_timer_get_fclk(timer_ptr);
-	gt_rate = clk_get_rate(gt_fclk);
-
-	/*Start the timer!*/
-	omap_dm_timer_start(timer_ptr);
-
-	printk(KERN_INFO "GP Timer initialized and started (%lu Hz, IRQ %d)\n",
-		(long unsigned)gt_rate, timer_irq);
+	printk("\n successfully disposed all irqs");
 
 	return 0;
 }
 
-static void __exit gptimer_request_exit(void)
+static int raw_allocation(void *unused)
 {
-	/*Stop the timer*/
-	omap_dm_timer_stop(timer_ptr);
+	struct device_node *np;
+	int i = 0;
+	int irq;
 
-	/*Release the timer*/
-	omap_dm_timer_free(timer_ptr);
+	printk("\n testing raw allocation");
 
-	printk(KERN_INFO "GP Timer finalized and stoped\n");
+	for (i = 0; i < (MAX_IRQS-32); i++)
+	{
+		np = platdev[i]->dev.of_node;
+		irq = irq_of_parse_and_map(np, 0);
+	}
+
+	printk("\n successfully allocated all irqs");
+
+	return 0;
 }
 
-module_init(gptimer_request_init);
-module_exit(gptimer_request_exit);
+static int test_allocation(void *unused)
+{
+	int irq;
+	int i = 0;
+	struct irq_desc *desc;
+	struct device_node *np;
+	int init_virq;
+
+	for (i = 0; i < (MAX_IRQS-32); i++)
+	{
+		np = platdev[i]->dev.of_node;
+
+		irq = irq_of_parse_and_map(np, 0);
+
+		if (i == 0)
+			init_virq = irq;
+
+		printk("\n **********");
+		printk("\n virq=%d for %d", irq, i);
+		desc = irq_to_desc(irq);
+
+		if (desc && irq && (irq < init_virq)) {
+			used_irqs[i] = 1;
+			printk("\n irq=%d already used by %d", desc->irq_data.hwirq, i);
+		}
+
+		if (!irq) {
+			printk("\n irq allocation failed for %d", i);
+			continue;
+		}
+
+		descs[i] = desc;
+
+		if (desc)
+			printk("\n %s is allocated to irq = %d", platdev[i]->name, desc->irq_data.hwirq);
+		else
+			printk("\n no irq allocated for %s", platdev[i]->name);
+
+	}
+
+	return 0;
+}
+
+static int test_allocate_deletion(void)
+{
+	int i, j = 100;
+	struct task_struct *p[2];
+
+	test_allocation(&i);
+	delete_allocation(&i);
+
+	while(j--) {
+		printk("\n loop %d", j);
+		p[0] = kthread_create(test_allocation, &i, "cb_test_thread");
+		kthread_bind(p[0], 0);
+
+		p[1] = kthread_create(delete_allocation, &i, "cb_test_thread");
+		kthread_bind(p[1], 1);
+
+		wake_up_process(p[0]);
+		wake_up_process(p[1]);
+	}
+
+	return 0;
+}
+
+static int option_get(void *data, u64 *v)
+{
+	u32 *option = data;
+
+	*v = *option;
+
+	return 0;
+}
+
+static int option_set(void *data, u64 v)
+{
+	test_allocate_deletion();
+
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(omap_cb_test_option_fops, option_get, option_set, "%llu\n");
+
+static int omap_cb_test_probe(struct platform_device *pdev) {
+
+	static int i = 0;
+
+	if (i == 0) {
+		d = debugfs_create_dir("cb-test", NULL);
+		if (IS_ERR_OR_NULL(d)) {
+			pr_err("OMAP cb-Test: Failed to create omap-cb-test directory!%d\n", d);
+			return PTR_ERR(d);
+		} else {
+			printk("\n created directory %d", d);
+		}
+
+		debugfs_create_file("cb", S_IRUGO | S_IWUSR, d,
+			&omap_cb_test_all, &omap_cb_test_option_fops);
+
+		d = debugfs_create_file("stress", S_IRUGO | S_IWUSR, d,
+			&stress, &omap_cb_test_option_fops);
+
+		printk("\n created file %d", d);
+	}
+
+	platdev[i++] = pdev;
+
+	return 0;
+}
+
+static int omap_cb_test_remove(struct platform_device *pdev) {
+
+	return 0;
+}
+
+static struct platform_driver omap_cb_driver = {
+	.probe	= omap_cb_test_probe,
+	.remove	= omap_cb_test_remove,
+	.driver = {
+		.name = "irq-cb-test",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(omap_cb_match),
+	},
+};
+
+static int omap_cb_init(void)
+{
+	platform_driver_register(&omap_cb_driver);
+
+	return 0;
+}
+module_init(omap_cb_init);
